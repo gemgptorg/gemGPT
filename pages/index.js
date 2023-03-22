28a15fe6f513f547c0f980f9c2a29269c4c28fbe
gemgptorg <gary@gemgpt.org>
2023-03-22 14:21:21 +0300

    favicon

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         <title>GemGPT</title>
         <meta name="description" content="GemGPT AI" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <main className={styles.main}>
       <Hero id="#home"></Hero>
